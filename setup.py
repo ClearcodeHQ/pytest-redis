cd067b7bca7407b6a58455a9b22157cfe7ea7468
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2021-05-17 18:20:21 +0200

    Move package definition from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -17,72 +17,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with pytest-redis. If not, see <http://www.gnu.org/licenses/>.
 """Installation module for pytest-redis."""
+from setuptools import setup
 
-import os
-from setuptools import setup, find_packages
-
-here = os.path.dirname(__file__)
-
-
-def read(fname):
-    """
-    Read given file's content.
-
-    :param str fname: file name
-    :returns: file contents
-    :rtype: str
-    """
-    return open(os.path.join(here, fname)).read()
-
-
-requirements = [
-    "pytest>=3.0.0",
-    "mirakuru>=0.2",  # test executors helpers
-    "port-for>=0.3.1",  # needed for random port selection
-    "redis",
-]
-
-test_requires = [
-    "pytest-cov",
-    "pytest-xdist",
-    "Mock",
-]
-
-extras_require = {"tests": test_requires}
 
 setup(
-    name="pytest-redis",
-    version="2.0.0",
-    description="Redis fixtures and fixture factories for Pytest.",
-    long_description=(read("README.rst") + "\n\n" + read("CHANGES.rst")),
-    keywords="tests py.test pytest fixture redis",
-    author="Clearcode - The A Room",
-    author_email="thearoom@clearcode.cc",
-    url="https://github.com/ClearcodeHQ/pytest-redis",
-    license="LGPLv3",
-    python_requires=">=3.5",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: " "GNU Lesser General Public License v3 or later (LGPLv3+)",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    install_requires=requirements,
-    tests_require=test_requires,
-    test_suite="tests",
-    include_package_data=True,
-    zip_safe=False,
-    extras_require=extras_require,
     entry_points={"pytest11": ["pytest_redis = pytest_redis.plugin"]},
 )
